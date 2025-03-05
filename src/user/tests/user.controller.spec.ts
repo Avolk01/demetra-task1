10899d7697465a830e2c768bef116eac9836d41e
Artem Volkodav <artem.volkodav@x5.ru>
2025-03-06 00:21:57 +0300

    fix: переделал тесты

--- src/user/tests/user.controller.spec.ts
+++ src/user/tests/user.controller.spec.ts
@@ -1,18 +1,44 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../controllers';
+import { UserService } from '../providers';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-    }).compile();
+  beforeEach(async (): Promise<void> => {
+    userService = {
+      register: jest.fn(),
+      login: jest.fn(),
+      getProfile: jest.fn(),
+      getUsers: jest.fn(),
+      updateUser: jest.fn(),
+      deleteUser: jest.fn(),
+    } as unknown as UserService;
 
-    controller = module.get<UserController>(UserController);
+    userController = new UserController(userService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  describe('register', (): void => {
+    it('Регистрация пользователя', async (): Promise<void> => {
+      const data = {
+        login: 'test',
+        email: 'test@test.com',
+        password: 'test',
+        age: 20,
+      };
+
+      const token = { access_token: 'token' };
+
+      jest.spyOn(userController, 'register').mockResolvedValue(token);
+
+      const res = await userController.register(data);
+
+      expect(userController.register).toHaveBeenCalledWith(data);
+      expect(res.access_token).toEqual(token.access_token);
+    });
   });
 });
--- src/user/tests/user.service.spec.ts
+++ src/user/tests/user.service.spec.ts
@@ -1,18 +1,51 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from '../providers';
+import { UserRepository, UserService } from '../providers';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from 'src/auth/providers';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let userRepository: UserRepository;
+  let configService: ConfigService;
+  let authService: AuthService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+  beforeEach(async (): Promise<void> => {
+    configService = {
+      get: jest.fn(),
+    } as unknown as ConfigService;
 
-    service = module.get<UserService>(UserService);
+    userRepository = {
+      findOneByLoginOrEmail: jest.fn(),
+      createUser: jest.fn(),
+    } as unknown as UserRepository;
+
+    authService = {
+      generateToken: jest.fn(),
+    } as unknown as AuthService;
+
+    userService = new UserService(authService, userRepository, configService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('register', (): void => {
+    it('Регистрация пользователя', async (): Promise<void> => {
+      const data = {
+        login: 'test',
+        email: 'test@test.com',
+        password: 'test',
+        age: 20,
+      };
+
+      const token = { access_token: 'token' };
+
+      jest.spyOn(userService, 'register').mockResolvedValue(token);
+
+      const res = await userService.register(data);
+
+      expect(userService.register).toHaveBeenCalledWith(data);
+      expect(res.access_token).toEqual(token.access_token);
+    });
   });
 });
