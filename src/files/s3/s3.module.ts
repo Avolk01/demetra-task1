74a81a6df93c49a9b2f3716c892f8851e0029818
Artem Volkodav <artem.volkodav@x5.ru>
2025-03-18 00:01:13 +0300

    feat: minio init

---
+++ src/files/s3/s3.module.ts
@@ -0,0 +1,27 @@
+import * as AWS from '@aws-sdk/client-s3';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { S3Lib } from './constants/do-spaces-service-lib.constant';
+import { S3Service } from './s3.service';
+
+@Module({
+  providers: [
+    S3Service,
+    {
+      provide: S3Lib,
+      useFactory: (configService: ConfigService) => {
+        return new AWS.S3({
+          endpoint: 'http://127.0.0.1:9000',
+          region: 'ru-central1',
+          credentials: {
+            accessKeyId: configService.get('S3_ACCESS_KEY_ID'),
+            secretAccessKey: configService.get('S3_SECRET_ACCESS_KEY'),
+          },
+        });
+      },
+    },
+  ],
+  exports: [S3Service, S3Lib],
+})
+export class S3Module {}
